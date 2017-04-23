4418092671ac1b41d6d6cca28c02ab6d8e4e5895
Alexey Fetisov <alfet123@gmail.com>
2017-04-23 16:14:08 +0500

    Отображение категорий и объявлений

--- index.php
+++ index.php
@@ -14,6 +14,19 @@ $now = time();
 // далее нужно вычислить оставшееся время до начала следующих суток и записать его в переменную $lot_time_remaining
 $lot_time_remaining = gmdate("H:i", $tomorrow - $now);
 
+// массив категорий
+$category = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+
+// массив объявлений
+$lots = [
+    ["name" => "2014 Rossignol District Snowboard", "category" => "Доски и лыжи", "price" => "10999", "image" => "img/lot-1.jpg"],
+    ["name" => "DC Ply Mens 2016/2017 Snowboard", "category" => "Доски и лыжи", "price" => "159999", "image" => "img/lot-2.jpg"],
+    ["name" => "Крепления Union Contact Pro 2015 года размер L/XL", "category" => "Крепления", "price" => "8000", "image" => "img/lot-3.jpg"],
+    ["name" => "Ботинки для сноуборда DC Mutiny Charocal", "category" => "Ботинки", "price" => "10999", "image" => "img/lot-4.jpg"],
+    ["name" => "Куртка для сноуборда DC Mutiny Charocal", "category" => "Одежда", "price" => "7500", "image" => "img/lot-5.jpg"],
+    ["name" => "Маска Oakley Canopy", "category" => "Разное", "price" => "5400", "image" => "img/lot-6.jpg"]
+];
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -79,20 +92,24 @@ $lot_time_remaining = gmdate("H:i", $tomorrow - $now);
             <h2>Открытые лоты</h2>
             <select class="lots__select">
                 <option>Все категории</option>
+                <?php foreach ($category as $key => $value): ?>
+                    <option><?=$value;?></option>
+                <?php endforeach; ?>
             </select>
         </div>
         <ul class="lots__list">
+            <?php foreach ($lots as $key => $value): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?=$value['image'];?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?=$value['category'];?></span>
+                    <h3 class="lot__title"><a class="text-link" href=""><?=$value['name'];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$value['price'];?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             <?=$lot_time_remaining;?>
@@ -100,6 +117,7 @@ $lot_time_remaining = gmdate("H:i", $tomorrow - $now);
                     </div>
                 </div>
             </li>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
