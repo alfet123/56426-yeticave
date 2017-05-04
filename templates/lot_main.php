45498b21dffee581cb358f1c69f32395bfdb1a0a
Alexey Fetisov <alfet123@gmail.com>
2017-05-04 15:52:50 +0500

    Скрытие блока добавления ставки

--- templates/lot_main.php
+++ templates/lot_main.php
@@ -28,6 +28,7 @@
                     равнодушным.</p>
             </div>
             <div class="lot-item__right">
+            <?php if (isset($_SESSION['user'])): ?>
                 <div class="lot-item__state">
                     <div class="lot-item__timer timer">
                         10:54:12
@@ -49,6 +50,7 @@
                         <button type="submit" class="button">Сделать ставку</button>
                     </form>
                 </div>
+            <?php endif; ?>
                 <div class="history">
                     <h3>История ставок (<span>4</span>)</h3>
                     <table class="history__list">
