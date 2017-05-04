6857766054cda0859d6bc3e39fe17457ea58f21e
Alexey Fetisov <alfet123@gmail.com>
2017-05-04 13:16:31 +0500

    Проверка заполнения полей

--- login.php
+++ login.php
@@ -4,6 +4,35 @@ require_once 'functions.php';
 
 // подключение файла с данными
 require_once 'data.php';
+
+// массив для данных из формы
+$formData = [
+    'email' => '',
+    'password' => ''
+];
+
+// массив для дополнительных классов
+$formClasses = [
+    'email' => '',
+    'password' => '',
+    'form' => ''
+];
+
+// проверка, что была отправка формы
+if (isset($_POST['send'])) {
+
+    //проверка пустых значений
+    foreach ($formData as $key => $value) {
+        if (empty($_POST[$key])) {
+            $formClasses['form'] = 'form--invalid';
+            $formClasses[$key] = 'form__item--invalid';
+        } else {
+            $formData[$key] = $_POST[$key];
+        }
+    }
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +47,7 @@ require_once 'data.php';
 
 <?=includeTemplate('templates/lot_header.php', []); ?>
 
-<?=includeTemplate('templates/login_main.php', ['categories' => $categories]); ?>
+<?=includeTemplate('templates/login_main.php', ['categories' => $categories, 'data' => $formData, 'class' => $formClasses]); ?>
 
 <?=includeTemplate('templates/footer.php', ['categories' => $categories]); ?>
 
