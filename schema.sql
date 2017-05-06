92d35d999dd4bd523a4bf724b163fc71ec8ad40f
Alexey Fetisov <alfet123@gmail.com>
2017-05-06 22:23:58 +0500

    Исправления

--- schema.sql
+++ schema.sql
@@ -1,45 +1,46 @@
 create table `category` (
-  `id` int auto_increment primary key,
-  `name` char(32)
+  `id` int unsigned not null auto_increment primary key,
+  `name` char(32) not null
 );
 
 create table `lot` (
-  `id` int auto_increment primary key,
-  `date_create` datetime,
-  `name` char(32),
-  `description` char(64),
-  `image` char(64),
-  `price` decimal,
+  `id` int unsigned not null auto_increment primary key,
+  `date_create` datetime not null,
+  `name` char(64) not null,
+  `description` char(128),
+  `image` char(255),
+  `price` decimal not null,
   `date_expire` datetime,
-  `step` decimal,
+  `step` decimal not null,
   `likes` int,
-  `owner` int,
-  `winner` int,
-  `category` int
+  `owner` int unsigned not null,
+  `winner` int unsigned,
+  `category` int unsigned not null
 );
 
 create table `bet` (
-  `id` int auto_increment primary key,
-  `date` datetime,
-  `price` decimal,
-  `user` int,
-  `lot` int
+  `id` int unsigned not null auto_increment primary key,
+  `date` datetime not null,
+  `price` decimal not null,
+  `user` int unsigned not null,
+  `lot` int unsigned not null
 );
 
 create table `user` (
-  `id` int auto_increment primary key,
-  `date_reg` datetime,
-  `email` char(32),
-  `name` char(32),
-  `password` char(32),
-  `avatar` char(64),
-  `contacts` char(64),
-  `lot` int,
-  `bet` int
+  `id` int unsigned not null auto_increment primary key,
+  `date_reg` datetime not null,
+  `email` char(32) not null,
+  `name` char(64) not null,
+  `password` char(32) not null,
+  `avatar` char(255),
+  `contacts` char(64) not null
 );
 
 create unique index `category_name` on `category`(`name`);
 create unique index `user_email` on `user`(`email`);
 
-create index `lot_name` on `lot`(`name`);
-create index `lot_description` on `lot`(`description`);
+create index `lot_category` on `lot`(`category`);
+create index `user_email` on `user`(`email`);
+create index `lot_owner` on `lot`(`owner`);
+create index `bet_user` on `bet`(`user`);
+create index `bet_lot` on `bet`(`lot`);
