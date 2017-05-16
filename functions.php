e7299e121353dc81e1afe75678de878c75df77e0
Alexey Fetisov <alfet123@gmail.com>
2017-05-16 10:45:31 +0500

    Вернул удаленные функции

--- functions.php
+++ functions.php
@@ -36,6 +36,16 @@ function includeTemplate($file, $data)
     return $result;
 }
 
+// функция получения аватара
+function getAvatar()
+{
+    if (isset($_SESSION['user']) && !empty($_SESSION['user']['avatar'])) {
+        return $_SESSION['user']['avatar'];
+    } else {
+        return 'img/user.jpg';
+    }
+}
+
 // функция выводит время в относительном формате
 function timeInRelativeFormat($ts)
 {
@@ -64,6 +74,244 @@ function getMaxBet($bets)
 {
     $betPrice = array_map(function($b) { return $b['price']; }, $bets);
     return (count($betPrice)) ? max($betPrice) : 0;
+}
+
+// функция проверки аутентификации
+function requireAuthentication()
+{
+    if (!isset($_SESSION['user'])) {
+        header("Location: login.php");
+        exit;
+    }
+}
+
+// функция для получения данных
+function getData($link, $sql, $data = [])
+{
+    $result = [];
+
+    $stmt = db_get_prepare_stmt($link, $sql, $data);
+
+    if ($stmt) {
+
+        if (mysqli_stmt_execute($stmt)) {
+
+            $res = mysqli_stmt_get_result($stmt);
+
+            while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+                $result[] = $row;
+            }
+
+        }
+
+        mysqli_stmt_close($stmt);
+
+    }
+
+    return $result;
+}
+
+// функция для добавления данных
+function insertData($link, $sql, $data)
+{
+    $result = false;
+
+    $stmt = db_get_prepare_stmt($link, $sql, $data);
+
+    if ($stmt) {
+
+        if (mysqli_stmt_execute($stmt)) {
+            $result = mysqli_stmt_insert_id($stmt);
+        }
+
+        mysqli_stmt_close($stmt);
+
+    }
+
+    return $result;
+}
+
+// функция создания фрагмента запроса
+function sqlFragment($data, $separator, &$values)
+{
+    $pairs = [];
+    foreach ($data as $key => $value) {
+        $pairs[] = "`".$key."` = ?";
+        $values[] = $value;
+    }
+    return implode($separator, $pairs);
+}
+
+// функция для обновления данных
+function updateData($link, $table, $data, $conditions)
+{
+    $result = false;
+
+    $values = [];
+
+    $sql = "update `".$table."` set ";
+
+    $sql .= sqlFragment($data, ", ", $values);
+
+    if (!empty($conditions)) {
+        $sql .= " where ".sqlFragment($conditions, " and ", $values);
+    }
+
+    $stmt = db_get_prepare_stmt($link, $sql, $values);
+
+    if ($stmt) {
+
+        if (mysqli_stmt_execute($stmt)) {
+            $result = mysqli_stmt_affected_rows($stmt);
+        }
+
+        mysqli_stmt_close($stmt);
+
+    }
+
+    return $result;
+}
+
+// функция для подключения к базе данных
+function dbConnect($db)
+{
+    $link = mysqli_connect($db['host'], $db['user'], $db['pass'], $db['name']);
+
+    if ($link) {
+        mysqli_query($link, "SET NAMES 'utf8'");
+        mysqli_query($link, "SET CHARACTER SET 'utf8'");
+    }
+
+    return $link;
+}
+
+// функция получения пользователя по e-mail
+function getUserByEmail($link, $email)
+{
+    $sql = 'select * from `user` where `email` = ? limit 1';
+
+    $data = getData($link, $sql, [$email]);
+
+    if (!empty($data)) {
+        return $data[0];
+    }
+
+    return null;
+}
+
+// функция получения пользователя по Id
+function getUserById($link, $userId)
+{
+    $sql = 'select * from `user` where `id` = ?';
+
+    $data = getData($link, $sql, [$userId]);
+
+    if (!empty($data)) {
+        return $data[0];
+    }
+
+    return null;
+}
+
+// функция получения категорий
+function getCategories($link)
+{
+    $sql = 'select `id`, `name` from `category` order by `id`';
+
+    return getData($link, $sql, []);
+}
+
+// функция получения лотов
+function getLots($link, array $ids = [])
+{
+    $sql  = 'select lot.*, category.name as category ';
+    $sql .= 'from lot ';
+    $sql .= 'join category on lot.category = category.id ';
+    if (!empty($ids)) {
+        $sql .= 'where lot.id in ('.implode(',', array_fill(0, count($ids), '?')).') ';
+    }
+    $sql .= 'order by date_create desc';
+
+    return getData($link, $sql, $ids);
+}
+
+// функция получения лота по Id
+function getLotById($link, $lotId)
+{
+    $data = getLots($link, [$lotId]);
+
+    if (!empty($data)) {
+        return $data[0];
+    }
+
+    return null;
+}
+
+// функция получения списка ставок по лоту с сортировкой по убыванию цены
+function getBetsByLot($link, $lotId)
+{
+    $sql  = 'select bet.date, bet.price, user.id, user.name ';
+    $sql .= 'from bet ';
+    $sql .= 'join user on bet.user = user.id ';
+    $sql .= 'where bet.lot = ? ';
+    $sql .= 'order by bet.price desc';
+
+    return getData($link, $sql, [$lotId]);
+}
+
+// функция получения списка ставок по пользователю с сортировкой по убыванию даты
+function getBetsByUser($link, $userId)
+{
+    $sql  = 'select lot.id, lot.image, lot.name as name, category.name as category, max(bet.price) as price, bet.date ';
+    $sql .= 'from lot ';
+    $sql .= 'join category on lot.category = category.id ';
+    $sql .= 'join bet on lot.id = bet.lot ';
+    $sql .= 'where bet.user = ? ';
+    $sql .= 'group by lot.id, bet.id ';
+    $sql .= 'order by bet.date desc';
+
+    return getData($link, $sql, [$userId]);
+}
+
+// функция добавления лота
+function newLot($link, array $lot)
+{
+    $sql  = 'insert into lot set ';
+    $sql .= 'date_create = ?, ';
+    $sql .= 'name = ?, ';
+    $sql .= 'description = ?, ';
+    $sql .= 'image = ?, ';
+    $sql .= 'price = ?, ';
+    $sql .= 'date_expire = ?, ';
+    $sql .= 'step = ?, ';
+    $sql .= 'owner = ?, ';
+    $sql .= 'category = ?';
+
+    return insertData($link, $sql, $lot);
+}
+
+// функция добавления ставки
+function newBet($link, array $bet)
+{
+    $sql  = 'insert into bet set ';
+    $sql .= 'date = ?, ';
+    $sql .= 'price = ?, ';
+    $sql .= 'user = ?, ';
+    $sql .= 'lot = ?';
+
+    return insertData($link, $sql, $bet);
+}
+
+// функция добавления пользователя
+function newUser($link, array $userData)
+{
+    $values = [];
+
+    $sql  = 'insert into user set ';
+
+    $sql .= sqlFragment($userData, ", ", $values);
+
+    return insertData($link, $sql, $values);
 }
 
 ?>
