f9b7fea3618ab39bd2c4eb6dbf51a1446e01dd89
Alexey Fetisov <alfet123@gmail.com>
2017-05-25 20:36:50 +0500

    Фильтрация лотов по категории

--- index.php
+++ index.php
@@ -5,11 +5,26 @@ require_once 'autoload.php';
 
 $categories = CategoryFinder::getAll();
 
-$lots = LotFinder::getLots();
+$category = null;
+
+if (isset($_GET['category'])) {
+    foreach ($categories as $key => $value) {
+        if ($value->id == $_GET['category']) {
+            $category = $value;
+            break;
+        }
+    }
+}
+
+if ($category) {
+    $lots = LotFinder::getLotsByCategory($category->id);
+} else {
+    $lots = LotFinder::getLots();
+}
 
 $templates = [
     'header' => ['avatar' => Auth::getAvatar()],
-    'main' => ['categories' => $categories, 'lots' => $lots, 'lot_time_remaining' => timeRemaining()],
+    'main' => ['categories' => $categories, 'cat_name' => $category ? '('.$category->name.')' : '', 'lots' => $lots, 'lot_time_remaining' => timeRemaining()],
     'footer' => ['categories' => $categories]
 ];
 
